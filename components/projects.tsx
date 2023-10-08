8d17a36df2e2b31e8175658b26dd3505f7932629
NikaA3 <abuashvili.nika@yahoo.com>
2023-10-08 21:41:37 +0400

    creating portoflio

---
+++ components/projects.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useEffect } from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./Project";
+import { useSecionInView } from "@/lib/hooks";
+
+export default function Projects() {
+  const { ref } = useSecionInView("Projects", 0.5);
+  return (
+    <section id="projects" className="scroll-mt-28 mb-28" ref={ref}>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ components/skills.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { skillsData } from "@/lib/data";
+import { useSecionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+export default function Skills() {
+  const { ref } = useSecionInView("Skills");
+
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+  return (
+    <section
+      id={"skills"}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+      ref={ref}
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            key={index}
+            className="
+					bg-white border border-black/[0.1] rounded-xl px-5 py-3
+					"
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+}
