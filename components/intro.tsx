2ef8af64222c0d5224ad13531815a964e7bc0e75
NikaA3 <abuashvili.nika@yahoo.com>
2023-10-15 15:01:09 +0400

    fixed bug

--- components/intro.tsx
+++ components/intro.tsx
@@ -60,7 +60,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I am Ricardo.</span> I am a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
